7bbe094475c34fd72eb6138b2b8217eb20f6a1af
TonyLegz <mleicvag@gmail.com>
2025-03-20 19:46:15 -0600

    Se cargo el proyecto

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Gestor de Inventario de Tienda ");
+        System.out.println("Prueba de enlace");
+        System.out.println("Otra prueba");
+
+    }
+}
